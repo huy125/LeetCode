aa98687c82c6c21e735e092b0b5ff39fe1582d96
DINH HUY NGUYEN <dinhhuypfiev@gmail.com>
2023-05-30 21:22:11 +0200

    Time: 14 ms (20.16%), Space: 18.9 MB (97.54%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.php
+++ 0020-valid-parentheses/0020-valid-parentheses.php
@@ -4,29 +4,17 @@ class Solution {
      * @param String $s
      * @return Boolean
      */
-    function isValid($s) {
+    function isValid(string $s): bool {
         $stack = [];
-        $openParentheses = ['(', '[', '{'];
-        $closeParentheses = [')', ']', '}'];
+        $parentheseSet = ['(' => ')', '[' => ']', '{' => '}'];
         for ($i = 0; $i < strlen($s); $i++) {
-            if (in_array($s[$i], $openParentheses)) {
-                array_unshift($stack, $s[$i]);
-            } else {
-                $previousParenthese = $stack[0];
-                if (
-                    array_search($previousParenthese, $openParentheses) === array_search($s[$i], $closeParentheses)
-                ) {
-                    array_shift($stack);
-                } else {
-                    return false;
-                }
+            if (array_key_exists($s[$i], $parentheseSet)) {
+                $stack[] = $parentheseSet[$s[$i]];
+            } elseif (array_pop($stack) !== $s[$i]) {
+                return false;
             }
         }
 
-        if (!empty($stack)) {
-            return false;
-        }
-
-        return true;
+        return count($stack) === 0;
     }
 }

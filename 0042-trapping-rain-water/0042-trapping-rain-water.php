5b499bfdc4968ee53be540614a4bd7a5f71647cb
DINH HUY NGUYEN <dinhhuypfiev@gmail.com>
2023-07-29 13:07:45 +0200

    Time: 24 ms (99.52%), Space: 22.1 MB (43.48%) - LeetHub

---
+++ 0042-trapping-rain-water/0042-trapping-rain-water.php
@@ -0,0 +1,34 @@
+class Solution {
+
+    /**
+     * @param Integer[] $height
+     * @return Integer
+     */
+    function trap($height) {
+        $left = 0;
+        $right = count($height) - 1;
+        $maxLeft = 0;
+        $maxRight = 0;
+        $trappedWater = 0;
+
+        while ($left < $right) {
+            if ($height[$left] < $height[$right]) {
+                if ($height[$left] >= $maxLeft) {
+                    $maxLeft = $height[$left];
+                } else {
+                    $trappedWater += $maxLeft - $height[$left]; 
+                }
+                $left++;
+            } else {
+                if ($height[$right] >= $maxRight) {
+                    $maxRight = $height[$right];
+                } else {
+                    $trappedWater += $maxRight - $height[$right];
+                }
+                $right--;
+            }
+        }
+
+        return $trappedWater;
+    }
+}

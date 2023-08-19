20eb95b8ba064da43b61a7871f8f426f5eadec6a
DINH HUY NGUYEN <dinhhuypfiev@gmail.com>
2023-08-19 12:28:54 +0200

    Time: 12 ms (18.92%), Space: 19.1 MB (42.7%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.php
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.php
@@ -6,7 +6,32 @@ class Solution {
      * @return Integer
      */
     function strStr($haystack, $needle) {
+        return $this->windowSlidingSolution($haystack, $needle);
+    }
+
+    function buildInPhpSolution($haystack, $needle): int
+    {
         $result = strpos($haystack, $needle);
+
         return !is_numeric($result) ? -1 : $result;
+    }
+
+    function windowSlidingSolution($haystack, $needle): int
+    {
+        $needleLength = strlen($needle);
+        $haystackLength = strlen($haystack);
+        for ($windowStart = 0; $windowStart <= $haystackLength - $needleLength; $windowStart++) {
+            for ($i = 0; $i < $needleLength; $i++) {
+                if ($haystack[$windowStart + $i] !== $needle[$i]) {
+                    break;
+                }
+
+                if ($i === $needleLength - 1) {
+                    return $windowStart;
+                }
+            }
+        }
+
+        return -1;
     }
 }

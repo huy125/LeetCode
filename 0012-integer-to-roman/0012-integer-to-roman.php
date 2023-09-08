1d4d93aeaed46df7c5de918f81d8fdc9b7f9bfa2
DINH HUY NGUYEN <dinhhuypfiev@gmail.com>
2023-09-08 17:40:17 +0200

    Time: 14 ms (77.24%), Space: 19.2 MB (40.65%) - LeetHub

---
+++ 0012-integer-to-roman/0012-integer-to-roman.php
@@ -0,0 +1,56 @@
+class Solution {
+
+    /**
+     * @param Integer $num
+     * @return String
+     */
+    function intToRoman($num) {
+        $memo = [
+            'M' => 1000,
+            'CM' => 900,
+            'D' => 500,
+            'CD' => 400,
+            'C' => 100,
+            'XC' => 90,
+            'L' => 50,
+            'XL' => 40,
+            'X' => 10,
+            'IX' => 9,
+            'V' => 5,
+            'IV' => 4,
+            'I' => 1,
+        ];
+
+        $dividend = $num;
+        $romanNumber = '';
+        foreach ($memo as $key => $value) {
+            $remainder = $dividend % $value;
+            $quotient = intval($dividend / $value);
+            $dividend = $remainder;
+
+            if (!empty($quotient)) {
+                $romanNumber = $this->buildRomanNumber($romanNumber, $key, $quotient);
+            }
+
+            if (empty($remainder)) {;
+                break;
+            }
+        }
+
+        return $romanNumber;
+    }
+
+    /**
+     * @param string $romanNumber
+     * @param string $letter
+     * @param int $numberLetters
+     */
+    function buildRomanNumber(string $romanNumber, string $letter, int $numberLetters): string
+    {
+        for ($i = 0; $i < $numberLetters; $i++) {
+            $romanNumber .= $letter;
+        }
+
+        return $romanNumber;
+    }
+}

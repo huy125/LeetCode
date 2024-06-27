6d8ca9bc978a05d3dd92901cc1b1faa4128ccea3
FirePhoenix <dinhhuypfiev@gmail.com>
2024-06-27 22:36:38 +0200

    Time: 53 ms (62.6%), Space: 48.9 MB (72.82%) - LeetHub

---
+++ 0957-minimum-add-to-make-parentheses-valid/0957-minimum-add-to-make-parentheses-valid.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var minAddToMakeValid = function(s) {
+    const stack = [];
+    let ans = 0;
+
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] == '(') {
+            stack.push(s[i]);
+        } else {
+            if (stack.length != 0 && stack[stack.length - 1] == '(') {
+                stack.pop();
+            } else {
+                ans++;
+            }
+        }
+    }
+
+    ans += stack.length;
+
+    return ans;
+};
